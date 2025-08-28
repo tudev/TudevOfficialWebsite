f3e1a7957ed787d42b6c2412120aa5ed10b49f49
root <root@23-92-17-204.ip.linodeusercontent.com>
2025-08-28 20:16:15 +0000

    Updated events page cards

--- app/events/page.js
+++ app/events/page.js
@@ -22,6 +22,37 @@ import SecondaryHeader from "../_components/headers/SecondaryHeader";
 const events = EVENTS2;
 
 function EventPage() {
+  const [upcomingEvents, setEvents] = useState([]);
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setLoading(true);
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_CMS_URL}/items/events?fields=*&filter[status][_eq]=published`
+        );
+
+        if (!res.ok) {
+          throw new Error(`Http error! status: ${res.status}`)
+        }
+
+        const data = await res.json();
+        console.log("This is the data fetched: ", data.data)
+        setEvents(data.data)
+        setError(null)
+      } catch (err) {
+        console.error("Failed to fetch events:", err);
+        setError("Unable to load events. Please try again later.")
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchEvents();
+  }, [])
+
+
   return (
     <>
       <SecondaryHeader title="Events" subtitle="What is happening" />
@@ -32,11 +63,46 @@ function EventPage() {
           <Row className="mb-4">
             <Col lg={12}>
               <h2>Upcoming Events</h2>
-              {/* <p>Please use the search bar to find projects</p> */}
             </Col>
           </Row>
           <Row>
-            {events.length == 0 ? (
+            {loading && (
+              <Col className="text-center mb-4">
+                <p>Loading ...</p>
+              </Col>
+            )}
+            {error && (
+              <Col className="text-center mb-4">
+                <p>{error}</p>
+              </Col>
+            )}
+            {!loading && !error && upcomingEvents.length === 0 && (
+              <Col className="text-center mb-4">
+                <h4>Currently No Events</h4>
+                <p>Please check back later!</p>
+              </Col>
+            )}
+            {!loading && !error && upcomingEvents.length > 0 && (
+              (upcomingEvents.map((upcomingEvent) => {
+                return (
+                  <Col key={upcomingEvent.id}
+                    lg={4}
+                    md={6}
+                    sm={12}
+                    xs={12}
+                    className="mb-4">
+                    <Card className="w-100 mx-auto" style={{ maxWidth: "400px" }}>
+                      <CardBody>
+                        <CardTitle tag="h5">{upcomingEvent.title}</CardTitle>
+                        <CardText>{upcomingEvent.description}</CardText>
+                        <CardText>{format(new Date(upcomingEvent.start_time), "EEEE MMMM do yyyy h:mma")}</CardText>
+                      </CardBody>
+                    </Card>
+                  </Col>
+                );
+              }))
+            )}
+            {/* {events.length == 0 ? (
               <Col className="text-center mb-4">
                 <h4>Currently No Events</h4>
                 <p>Please check back later!</p>
@@ -55,7 +121,7 @@ function EventPage() {
                   </Col>
                 );
               })
-            )}
+            )} */}
           </Row>
         </Container>
       </section>
